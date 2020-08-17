f2e23275d0ee06426952d330c1c26ee9f9d50596
Kobi <kobimiz2001@gmail.com>
2020-08-17 16:22:51 +0300

    fixed loading on homepage and now there is an alert before logging out

--- lib/services/http.services.dart
+++ lib/services/http.services.dart
@@ -5,68 +5,74 @@ import 'package:http/http.dart' as http;
 class HttpServices {
   static bool _isDebug = true;
   static String _devURL = 'http://192.168.1.17:8810';
-  static Map<String, String> _defaultHeaders = { 'Content-Type': 'application/json' };
-  
+  static Map<String, String> _defaultHeaders = {
+    'Content-Type': 'application/json'
+  };
+
   static String get serverURL {
-   return _isDebug ? 
-      _devURL
-    : 
-      // The production server
-      'https://real-server.com';
+    return _isDebug
+        ? _devURL
+        :
+        // The production server
+        'https://real-server.com';
   }
 
   static Future<UserCred> _prepareCredentials() async {
     try {
-      Map<String, dynamic> cred = await StorageH.getJSON(StoragePaths.LoginCred);
+      Map<String, dynamic> cred =
+          await StorageH.getJSON(StoragePaths.LoginCred);
       UserCred userCred = UserCred.fromJSON(cred);
 
-      if(userCred.expDate > DateTime.now().millisecondsSinceEpoch) {
-        await login(emailPass: { 'email': userCred.email, 'password': userCred.password });
-        Map<String, dynamic> newCredJSON = await StorageH.getJSON(StoragePaths.LoginCred);
+      if (userCred.expDate > DateTime.now().millisecondsSinceEpoch) {
+        await login(emailPass: {
+          'email': userCred.email,
+          'password': userCred.password
+        });
+        Map<String, dynamic> newCredJSON =
+            await StorageH.getJSON(StoragePaths.LoginCred);
         UserCred newCred = UserCred.fromJSON(newCredJSON);
 
         return newCred;
       }
 
       return userCred;
-    } catch(ex) {
+    } catch (ex) {
       throw ex;
     }
   }
 
-  static Future<bool> login({ Map<String, String> emailPass }) async {
+  static Future<bool> login({Map<String, String> emailPass}) async {
     try {
-      http.Response res = await http.post('$serverURL/app/login', body: emailPass);
-      if(res.statusCode == ResponseStatus.Ok) {
-        Map<String, dynamic> resBody = jsonDecode(res.body);      
+      http.Response res =
+          await http.post('$serverURL/app/login', body: emailPass);
+      if (res.statusCode == ResponseStatus.Ok) {
+        Map<String, dynamic> resBody = jsonDecode(res.body);
 
-        if(resBody['auth'].toString().toLowerCase() == 'true') {
+        if (resBody['auth'].toString().toLowerCase() == 'true') {
           Map<String, dynamic> creds = resBody['cred'];
           creds['expDate'] = resBody['expDate'];
           creds['email'] = emailPass['email'];
           creds['password'] = emailPass['password'];
 
-          StorageH.setJSON(StoragePaths.LoginCred, UserCred.fromJSON(creds).toJSON());
+          StorageH.setJSON(
+              StoragePaths.LoginCred, UserCred.fromJSON(creds).toJSON());
           return true;
         }
       }
-    } catch(ex) {
+    } catch (ex) {
       print(ex);
     }
     return false;
   }
 
-  static Future<NewUserCred> signup({ NewUserCred userCred }) async {
-    http.Response res = await http.post(
-      '$serverURL/app/sign-up',
-      headers: _defaultHeaders,
-      body: jsonEncode(userCred.toJSON())
-    );
+  static Future<NewUserCred> signup({NewUserCred userCred}) async {
+    http.Response res = await http.post('$serverURL/app/sign-up',
+        headers: _defaultHeaders, body: jsonEncode(userCred.toJSON()));
 
-    if(res.statusCode == ResponseStatus.Ok) {
+    if (res.statusCode == ResponseStatus.Ok) {
       Map<String, dynamic> resBody = jsonDecode(res.body);
 
-      if(resBody['isCreated'].toString().toLowerCase() == 'true'){
+      if (resBody['isCreated'].toString().toLowerCase() == 'true') {
         return NewUserCred.fromJSON(resBody['user']);
       }
     }
@@ -77,48 +83,47 @@ class HttpServices {
   static Future<double> getBalance() async {
     try {
       UserCred creds = await _prepareCredentials();
-      http.Response res = await http.post('$serverURL/app/get-balance', 
-        headers: _defaultHeaders,
-        body: jsonEncode(_buildCreds(creds))
-      );
+      http.Response res = await http.post('$serverURL/app/get-balance',
+          headers: _defaultHeaders, body: jsonEncode(_buildCreds(creds)));
 
-      if(res.statusCode == ResponseStatus.Ok) {
+      if (res.statusCode == ResponseStatus.Ok) {
         Map<String, dynamic> body = jsonDecode(res.body);
-        if(body != null) {
+        if (body != null) {
           List data = body['d'];
-          if(data.length > 0) {
+          if (data.length > 0) {
             BalanceResponse balance = BalanceResponse.fromJSON(body['d'][0]);
             return balance.current;
           } else {
             return 0;
           }
         }
-        
+
         return null;
       }
 
       throw "Wasn't able to get balance";
-    } catch(ex) {
+    } catch (ex) {
       throw ex;
     }
   }
 
-  static Future<bool> transferMoney(String destAccID, double amount, String pinCode) async {
+  static Future<bool> transferMoney(
+      String destAccID, double amount, String pinCode) async {
     try {
       UserCred creds = await _prepareCredentials();
-      http.Response res = await http.post('$serverURL/app/transfer-amount', headers: _defaultHeaders,
-        body: jsonEncode({
-          ..._buildCreds(creds),
-          'destAccID': destAccID,
-          'amount': amount,
-          'pinCode': pinCode,
-          'currencyIso': "ILS",
-          'text': 'Message'
-        })
-      );
+      http.Response res = await http.post('$serverURL/app/transfer-amount',
+          headers: _defaultHeaders,
+          body: jsonEncode({
+            ..._buildCreds(creds),
+            'destAccID': destAccID,
+            'amount': amount,
+            'pinCode': pinCode,
+            'currencyIso': "ILS",
+            'text': 'Message'
+          }));
 
       return res.statusCode == ResponseStatus.Ok;
-    } catch(ex) {
+    } catch (ex) {
       print('http ex, $ex');
     }
 
@@ -135,26 +140,26 @@ class HttpServices {
     };
   }
 
-  static Future<List<Page>> getRows({ 
-    String currencyIso = 'ILS', 
-    int pageNumber = 1, 
-    int pageSize = 20 
-  }) async {
+  static Future<List<Page>> getRows(
+      {String currencyIso = 'ILS',
+      int pageNumber = 1,
+      int pageSize = 20}) async {
     try {
       UserCred creds = await _prepareCredentials();
-      http.Response res = await http.post('$serverURL/app/get-rows', headers: _defaultHeaders, 
-        body: jsonEncode({
-          ..._buildCreds(creds), 'filters': { 'CurrencyIso': currencyIso }, 
-          'sortAndPage': { 'PageNumber': pageNumber, 'PageSize': pageSize }
-        }
-      ));
+      http.Response res = await http.post('$serverURL/app/get-rows',
+          headers: _defaultHeaders,
+          body: jsonEncode({
+            ..._buildCreds(creds),
+            'filters': {'CurrencyIso': currencyIso},
+            'sortAndPage': {'PageNumber': pageNumber, 'PageSize': pageSize}
+          }));
 
-      if(res.statusCode == ResponseStatus.Ok) {
+      if (res.statusCode == ResponseStatus.Ok) {
         Map<String, dynamic> resBody = jsonDecode(res.body);
         List pagesBuffer = resBody['data'];
         List<Page> toReturn = [];
 
-        for(Map pageJson in pagesBuffer) {
+        for (Map pageJson in pagesBuffer) {
           toReturn.add(Page.fromJSON(pageJson));
         }
 
@@ -162,7 +167,7 @@ class HttpServices {
       }
 
       return null;
-    } catch(ex) {
+    } catch (ex) {
       throw ex;
     }
   }
@@ -192,46 +197,60 @@ class Page {
   String text;
   double total;
 
-  Page({ 
-    this.amount, this.balanceTransferType, 
-    this.balanceTransferTypeName, this.comment, 
-    this.currencyIso, this.id, this.insertDate,
-    this.isPending, this.sourceAccountID, 
-    this.sourceAccountName, this.sourceAccountProfileImage, 
-    this.sourceAccountProfileImageSize,
-    this.sourceAccountType, this.sourceID, this.sourceType, 
-    this.targetAccountID, this.targetAccountName, this.targetAccountProfileImage, 
-    this.targetAccountProfileImageSize, this.targetAccountType, 
-    this.text, this.total
-  });
+  Page(
+      {this.amount,
+      this.balanceTransferType,
+      this.balanceTransferTypeName,
+      this.comment,
+      this.currencyIso,
+      this.id,
+      this.insertDate,
+      this.isPending,
+      this.sourceAccountID,
+      this.sourceAccountName,
+      this.sourceAccountProfileImage,
+      this.sourceAccountProfileImageSize,
+      this.sourceAccountType,
+      this.sourceID,
+      this.sourceType,
+      this.targetAccountID,
+      this.targetAccountName,
+      this.targetAccountProfileImage,
+      this.targetAccountProfileImageSize,
+      this.targetAccountType,
+      this.text,
+      this.total});
 
   static Page fromJSON(Map<String, dynamic> json) {
     try {
       return Page(
-        amount: int.parse(json['Amount'].toString()),
-        balanceTransferType: json['BalanceTransferType'].toString(),
-        balanceTransferTypeName: json['BalanceTransferTypeName'].toString(),
-        comment: json['Comment'].toString(),
-        currencyIso: json['CurrencyIso'].toString(),
-        id: int.parse(json['ID'].toString()),
-        insertDate: json['InsertDate'].toString(),
-        isPending: json['IsPending'].toString().toLowerCase() == 'true',
-        sourceAccountID: int.parse(json['SourceAccountID'].toString()),
-        sourceAccountName: json['SourceAccountName'].toString(),
-        sourceAccountProfileImage: json['SourceAccountProfileImage'].toString(),
-        sourceAccountProfileImageSize: int.parse(json['SourceAccountProfileImageSize'].toString()),
-        sourceAccountType: json['SourceAccountType'].toString(),
-        sourceID: int.parse(json['SourceID'].toString()),
-        sourceType: json['SourceType'].toString(),
-        targetAccountID: int.parse(json['TargetAccountID'].toString()),
-        targetAccountName: json['TargetAccountName'].toString(),
-        targetAccountProfileImage: json['TargetAccountProfileImage'].toString(),
-        targetAccountProfileImageSize: int.parse(json['TargetAccountProfileImageSize'].toString()),
-        targetAccountType: json['TargetAccountType'].toString(),
-        text: json['Text'].toString(),
-        total: double.parse(json['Total'].toString())
-      );
-    } catch(ex) {
+          amount: int.parse(json['Amount'].toString()),
+          balanceTransferType: json['BalanceTransferType'].toString(),
+          balanceTransferTypeName: json['BalanceTransferTypeName'].toString(),
+          comment: json['Comment'].toString(),
+          currencyIso: json['CurrencyIso'].toString(),
+          id: int.parse(json['ID'].toString()),
+          insertDate: json['InsertDate'].toString(),
+          isPending: json['IsPending'].toString().toLowerCase() == 'true',
+          sourceAccountID: int.parse(json['SourceAccountID'].toString()),
+          sourceAccountName: json['SourceAccountName'].toString(),
+          sourceAccountProfileImage:
+              json['SourceAccountProfileImage'].toString(),
+          sourceAccountProfileImageSize:
+              int.parse(json['SourceAccountProfileImageSize'].toString()),
+          sourceAccountType: json['SourceAccountType'].toString(),
+          sourceID: int.parse(json['SourceID'].toString()),
+          sourceType: json['SourceType'].toString(),
+          targetAccountID: int.parse(json['TargetAccountID'].toString()),
+          targetAccountName: json['TargetAccountName'].toString(),
+          targetAccountProfileImage:
+              json['TargetAccountProfileImage'].toString(),
+          targetAccountProfileImageSize:
+              int.parse(json['TargetAccountProfileImageSize'].toString()),
+          targetAccountType: json['TargetAccountType'].toString(),
+          text: json['Text'].toString(),
+          total: double.parse(json['Total'].toString()));
+    } catch (ex) {
       throw 'generate page from json ex: $ex';
     }
   }
@@ -270,7 +289,8 @@ class BalanceResponse {
   double expected;
   double pending;
 
-  BalanceResponse({ this.currencyIso, this.current, this.expected, this.pending });
+  BalanceResponse(
+      {this.currencyIso, this.current, this.expected, this.pending});
 
   Map<String, dynamic> toJSON() {
     return {
@@ -284,12 +304,11 @@ class BalanceResponse {
   static BalanceResponse fromJSON(Map<String, dynamic> json) {
     try {
       return BalanceResponse(
-        current: double.parse(json['Current'].toString()), 
-        expected: double.parse(json['Expected'].toString()), 
-        currencyIso: json['CurrencyIso'].toString(),
-        pending: double.parse(json['Pending'].toString())
-      );
-    } catch(ex) {
+          current: double.parse(json['Current'].toString()),
+          expected: double.parse(json['Expected'].toString()),
+          currencyIso: json['CurrencyIso'].toString(),
+          pending: double.parse(json['Pending'].toString()));
+    } catch (ex) {
       throw ex;
     }
   }
@@ -299,20 +318,16 @@ class NewUserData {
   String password;
   String pinCode;
 
-  NewUserData({ this.password, this.pinCode });
+  NewUserData({this.password, this.pinCode});
 
   Map<String, String> toJSON() {
-    return {
-      'password': password,
-      'pinCode': pinCode
-    };
+    return {'password': password, 'pinCode': pinCode};
   }
 
   static NewUserData fromJSON(Map<String, dynamic> json) {
     return NewUserData(
-      password: json['password'].toString(),
-      pinCode: json['pinCode'].toString()
-    );
+        password: json['password'].toString(),
+        pinCode: json['pinCode'].toString());
   }
 }
 
@@ -325,15 +340,14 @@ class NewUserInfo {
   String lastname;
   String postalCode;
 
-  NewUserInfo({
-    this.addressLine,
-    this.city,
-    this.country,
-    this.email,
-    this.firstname,
-    this.lastname,
-    this.postalCode
-  });
+  NewUserInfo(
+      {this.addressLine,
+      this.city,
+      this.country,
+      this.email,
+      this.firstname,
+      this.lastname,
+      this.postalCode});
 
   Map<String, String> toJSON() {
     return {
@@ -349,14 +363,13 @@ class NewUserInfo {
 
   static NewUserInfo fromJSON(Map<String, dynamic> json) {
     return NewUserInfo(
-      addressLine: json['addressLine'].toString(),
-      city: json['city'].toString(),
-      country: json['country'].toString(),
-      email: json['email'].toString(),
-      firstname: json['firstname'].toString(),
-      lastname: json['lastname'].toString(),
-      postalCode: json['postalCode'].toString()
-    );
+        addressLine: json['addressLine'].toString(),
+        city: json['city'].toString(),
+        country: json['country'].toString(),
+        email: json['email'].toString(),
+        firstname: json['firstname'].toString(),
+        lastname: json['lastname'].toString(),
+        postalCode: json['postalCode'].toString());
   }
 
   @override
@@ -380,17 +393,12 @@ class NewUserCred {
   NewUserCred(this.data, this.info);
 
   Map<String, dynamic> toJSON() {
-    return {
-      'data': data.toJSON(),
-      'info': info.toJSON()
-    };
+    return {'data': data.toJSON(), 'info': info.toJSON()};
   }
 
   static NewUserCred fromJSON(Map<String, dynamic> json) {
     return NewUserCred(
-      NewUserData.fromJSON(json['data']), 
-      NewUserInfo.fromJSON(json['info'])
-    );
+        NewUserData.fromJSON(json['data']), NewUserInfo.fromJSON(json['info']));
   }
 
   @override
@@ -407,14 +415,13 @@ class User {
   String profileImage;
   int lastConnected;
 
-  User({ 
-    this.username, 
-    this.password, 
-    this.email,
-    this.name, 
-    this.profileImage, 
-    this.lastConnected 
-  });
+  User(
+      {this.username,
+      this.password,
+      this.email,
+      this.name,
+      this.profileImage,
+      this.lastConnected});
 
   Map<String, dynamic> toJSON() {
     return {
@@ -429,13 +436,12 @@ class User {
 
   static User fromJSON(Map<String, dynamic> json) {
     return User(
-      username: json['username'],
-      password: json['password'],
-      email: json['email'],
-      name: json['name'],
-      profileImage: json['profileImage'],
-      lastConnected: json['lastConnected']
-    );
+        username: json['username'],
+        password: json['password'],
+        email: json['email'],
+        name: json['name'],
+        profileImage: json['profileImage'],
+        lastConnected: json['lastConnected']);
   }
 
   @override
@@ -447,23 +453,17 @@ class User {
 class UserCred {
   // CoriunderCredentials
   String id;
-  String credentialsToken; 
+  String credentialsToken;
   String credentialsHeaderName;
-  
+
   int expDate;
-  
+
   // username password of client
   String email;
   String password;
 
-  UserCred(
-    this.credentialsToken, 
-    this.credentialsHeaderName, 
-    this.expDate, 
-    this.email, 
-    this.password,
-    this.id
-  );
+  UserCred(this.credentialsToken, this.credentialsHeaderName, this.expDate,
+      this.email, this.password, this.id);
 
   Map<String, dynamic> toJSON() {
     return {
@@ -479,14 +479,13 @@ class UserCred {
   static UserCred fromJSON(Map<String, dynamic> json) {
     try {
       return UserCred(
-        json['CredentialsToken'].toString(),
-        json['CredentialsHeaderName'].toString(), 
-        int.parse(json['expDate'].toString()),
-        json['email'],
-        json['password'],
-        json['id']
-      );
-    } catch(ex) {
+          json['CredentialsToken'].toString(),
+          json['CredentialsHeaderName'].toString(),
+          int.parse(json['expDate'].toString()),
+          json['email'],
+          json['password'],
+          json['id']);
+    } catch (ex) {
       throw ex;
     }
   }
